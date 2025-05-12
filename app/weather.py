0e02e5515dae8560b3de4fb0a63822fb8398abd5
KellyAineah <kellyainea99@gmail.com>
2025-05-12 11:04:23 +0300

    add .env and .gitignore files , create weather.py

--- app/weather.py
+++ app/weather.py
@@ -9,3 +9,33 @@ load_dotenv()
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+
+async def fetch_weather(city_name: str, units: str = 'metric'):
+    
+    # search parameters sent to the API
+    params ={
+        'q': city_name,
+        'appid': API_KEY,
+        'units': units
+    }
+    # create an asynchronous HTTP client
+    async with httpx.AsyncClient() as client:
+
+        # make a GET request to the OpenWeather API
+        response = await client.get(BASE_URL, params=params)
+
+        #if error occurs, raise an exception
+        response.raise_for_status()
+        
+        # convert the JSON response to a Python dictionary
+        data = response.json()
+        
+        # extract relevant information from the response
+        return {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+            'humidity': data['main']['humidity'],
+            'wind_speed': data['wind']['speed'],
+        }
